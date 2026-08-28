8054ae0a738691a0982e6a713088e7099b2191d1
Pramod prakash jadhav <pramodj551@gmail.com>
2026-08-28 11:32:19 +0530

    Add src/__init__.py to make src a proper Python package

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+"""
+==========================================================
+AI-Powered Cybersecurity ML Pipeline
+Part 2 - Package Initialization
+
+Author : Pramod Prakash Jadhav
+Python : 3.11+
+==========================================================
+"""
+
+__version__ = "2.0.0"
+__author__ = "Pramod Prakash Jadhav"
+__all__ = [
+    "config",
+    "data_loader",
+    "feature_selection",
+    "logger",
+    "model_evaluation",
+    "model_training",
+    "pipeline",
+    "predict",
+    "preprocessing",
+    "utils",
+    "run_pipeline",
+]
